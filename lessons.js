b5ac6b0b3306f24f737a5dc29653f65997aa06cd
Yuri <yuri_bilyarov@hotmail.co.uk>
2021-11-16 19:50:24 +0000

    displays all items

---
+++ lessons.js
@@ -0,0 +1,92 @@
+lessons = [
+    {
+        id: 1001,
+        subject: "Math",
+        location: "Barnet",
+        price: 20,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "math.png"
+    },
+    {
+        id: 1002,
+        subject: "Art",
+        location: "Enfield",
+        price: 15,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "art.png"
+    },
+    {
+        id: 1004,
+        subject: "Science",
+        location: "Barnet",
+        price: 25,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "science.png"
+    },
+    {
+        id: 1005,
+        subject: "Sports",
+        location: "Haringey",
+        price: 10,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "tennis.png"
+    },
+    {
+        id: 1006,
+        subject: "Geography",
+        location: "Harrow",
+        price: 20,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "geography.png"
+    },
+    {
+        id: 1007,
+        subject: "History",
+        location: "Harrow",
+        price: 20,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "history.png"
+    },
+    {
+        id: 1003,
+        subject: "English",
+        location: "Camden",
+        price: 10,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "english.png"
+    },
+    {
+        id: 1008,
+        subject: "Spanish",
+        location: "Camden",
+        price: 10,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "spanish.png"
+    },
+    {
+        id: 1009,
+        subject: "French",
+        location: "Camden",
+        price: 10,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "french.png"
+    },
+    {
+        id: 1010,
+        subject: "German",
+        location: "Camden",
+        price: 10,
+        availableInventory: 5,
+        fontAwesomeIcon: "",
+        image: "german.png"
+    },
+]
