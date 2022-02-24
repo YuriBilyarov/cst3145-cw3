dcbb39d3848f8784b824393148dad25c40c302e3
Yuri <yuri_bilyarov@hotmail.co.uk>
2022-02-24 20:01:06 +0000

    Adds cw3(group work) functinality

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+var cacheName = 'schoolcache';
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(function (r) {
+        console.log('[Service Worker] Download the file if it is not in the cache');
+        return r || fetch(e.request).then(function (response) {
+            console.log('[Service Worker] Add the new file to cache');
+            return caches.open(cacheName).then(function (cache) {
+                cache.put(e.request, response.clone());
+                return response;
+            });
+        });
+    })
+    );
+});
